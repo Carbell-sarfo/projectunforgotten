f59b3638f92a495d89b829b4e381ea64177b3b31
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-01-25 18:49:57 +0000

    sass folder set up

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {

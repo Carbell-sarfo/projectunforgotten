8cbe5e7f790a062b2b77e4a50341f26947af97df
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-02-15 18:22:05 +0000

    Back to work now 15-02-2023

---
+++ src/_data/meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // NOTE: `process.env.URL` is provided by Netlify, and may need
+  // adjusted pending your host
+  url: process.env.URL || "http://localhost:8080",
+  siteName: "Project Unforgotten",
+  siteDescription:
+    "We are committed to showing love toe the mentally challenged (the forgotten), living on the streetsWe are committed to showing love toe the mentally challenged (the forgotten), living on the streets",
+  authorName: "Carbell Sarfo", // optional
+};

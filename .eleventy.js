4edf8ba2d189011888fcb7f94426d3933994f7b8
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-03-23 15:50:27 +0000

    reverted eleventy.js file to old one

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,6 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
-  eleventyConfig.addPassthroughCopy("./src/assets/");
-  eleventyConfig.addPassthroughCopy("./src/js/");
 
   return {
     dir: {
